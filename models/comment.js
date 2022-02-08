7e6b35eb3096a408f54c7289031ceffaae8abb04
wiily <willyhuang18@gmail.com>
2022-02-07 21:19:04 -0800

    adding data to comment model

--- models/comment.js
+++ models/comment.js
@@ -7,6 +7,23 @@ class Comment extends Model{}
 
 Comment.init (
     {
-        
+       id:{
+           type: DataTypes.INTEGER,
+           primaryKey: true,
+           allowNull: false,
+           autoIncrement: true
+       },
+       comment_text: {
+           type:DataTypes.STRING,
+           allowNull:false
+       } 
+    },
+    {
+        sequelize,
+        freezeTableName:true,
+        underscored: true,
+        modelName:'comment'
     }
-)
+);
+
+module.exports = Comment;
