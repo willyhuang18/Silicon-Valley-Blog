d22197ee41eb78887e59b03ef5f99299e474a2ed
wiily <willyhuang18@gmail.com>
2022-02-07 17:09:44 -0800

    adding condition for deploying

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,16 @@ const Sequelize = require('sequelize');
 
 //import the dotenv
 require('dotenv').config();
+
+//adding condition for deploying
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
