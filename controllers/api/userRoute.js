ed5e12a4e8b9a1cf967f48847f65f8a1a2a1c1c3
wiily <willyhuang18@gmail.com>
2022-02-08 15:55:11 -0800

    put route

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -49,3 +49,23 @@ router.post('/', (req,res)=>{
         res.status(500).json(err);
     });
 });
+
+// put route for update user
+router.put('/:id', (req,res)=>{
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(response =>{
+        if(!response){
+            res.status(404).json({ message: 'Please enter valid user id'});
+            return;
+        }
+        res.json(response)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
