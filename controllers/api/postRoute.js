e07c8c486e12ffb8a40a429ec5e957737c2a3483
wiily <willyhuang18@gmail.com>
2022-02-07 23:26:39 -0800

    getting model

---
+++ controllers/api/postRoute.js
@@ -0,0 +1,4 @@
+//import router express
+const router = require('express').Router();
+//getting Post model
+const { Post } = require('../../models');
---
+++ controllers/api/userRoute.js
@@ -0,0 +1,4 @@
+//import router express
+const router = require('express').Router();
+//getting User model
+const { User } = require('../../models');
