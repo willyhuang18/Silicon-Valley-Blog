987d97d63411f6efd772ed26018651cabe780c3d
wiily <willyhuang18@gmail.com>
2022-02-07 21:37:26 -0800

    define columns for post model

---
+++ models/post.js
@@ -0,0 +1,32 @@
+//import the model from sequelize
+const { Model, DataTypes } = require('sequelize');
+//import connection file
+const sequelize = require('../config/connection');
+
+class Post extends Model{}
+
+//define columns
+Post.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len:[1]
+            }
+          },
+          text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+              len:[1]
+            }
+        }
+    }
+)
