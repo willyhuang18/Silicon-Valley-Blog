29c6d444288075eb4d99af18994f9282b9d279d7
wiily <willyhuang18@gmail.com>
2022-02-07 23:51:01 -0800

    //post route by create new user

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -35,3 +35,17 @@ router.get('/:id', (req,res) => {
         res.status(500).json(err);
     })
 })
+
+//post route by create new user'
+router.post('/', (req,res)=>{
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(response => res.json(response))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
