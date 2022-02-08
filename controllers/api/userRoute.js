cf78306e4d44c55a6cbb9524bbf4f05875ceaf20
wiily <willyhuang18@gmail.com>
2022-02-07 23:29:16 -0800

    using findall

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -2,3 +2,11 @@
 const router = require('express').Router();
 //getting User model
 const { User } = require('../../models');
+
+//Get all user
+router.get('/', (req, res)=>{
+    //using findAll() method 
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+})
