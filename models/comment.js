6b3837bf812e1b56872864fd5907d2b0caaf9d60
wiily <willyhuang18@gmail.com>
2022-02-07 21:03:33 -0800

    creating comment model

--- models/comment.js
+++ models/comment.js
@@ -2,3 +2,11 @@
 const { Model, DataTypes } = require('sequelize');
 //import connection file
 const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init (
+    {
+        
+    }
+)
