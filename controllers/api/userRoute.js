4ab7b46bda8939354bd04616749a5cdb94928d75
wiily <willyhuang18@gmail.com>
2022-02-08 15:56:41 -0800

    //delete route for user

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -69,3 +69,25 @@ router.put('/:id', (req,res)=>{
         res.status(500).json(err);
     })
 })
+
+//delete route for user
+router.delete('/:id', (req, res) =>{
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(response =>{
+        if(!response){
+            res.status(404).json({ message: 'Please enter valid user id'});
+            return;
+        }
+        res.json(response)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
