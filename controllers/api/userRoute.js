11877df782b8c0512107bf8da8a128a5cfddd68e
wiily <willyhuang18@gmail.com>
2022-02-08 16:16:19 -0800

    importing withAuth utils

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -2,6 +2,8 @@
 const router = require('express').Router();
 //getting User Post Comment model
 const { User, Post, Comment } = require('../../models');
+//importing withAuth utils
+const withAuth = require('../../utils/auth');
 
 //Get all user
 router.get('/', (req, res)=>{
