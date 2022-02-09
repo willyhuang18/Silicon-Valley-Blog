0561c74033b2e2dc43d177a7f25a07a434ddd403
wiily <willyhuang18@gmail.com>
2022-02-08 20:26:47 -0800

    call back

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -57,5 +57,15 @@ router.get('/:id', (req, res)=>{
             },
         ]
     })
-   
+    .then(response =>{
+            if(!response){
+                res.status(404).json({ message: 'Please enter valid user id'});
+                return;
+            }
+         res.json(response)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
