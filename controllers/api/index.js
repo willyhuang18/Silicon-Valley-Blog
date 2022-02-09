06351dd7c5d6c2e3c404608d88d57ce5c1ccccf2
wiily <willyhuang18@gmail.com>
2022-02-08 21:05:06 -0800

    //import routes

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+//import router express
+const router = require('express').Router();
+//import the other apis
+const user = require('./userRoute');
+const post = require('./postRoute');
+const comment = require('./commentRoute');
