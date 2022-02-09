d5150c6844681f8b0d9d1ea4f3c93ffa90fe4982
wiily <willyhuang18@gmail.com>
2022-02-08 21:08:28 -0800

    starting the server

--- sever.js
+++ sever.js
@@ -1,2 +1,6 @@
 //import express
 const express = require('express');
+//import controller api files
+const routes = require('./controllers');
+//import connection file
+const sequelize = require('./config/connection');
