852573692bb0df8480cdc7cb9c5bfc53a3be9350
wiily <willyhuang18@gmail.com>
2022-02-08 23:46:12 -0800

    //display post in the dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,3 +3,32 @@ const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const withAuth = require('../utils/auth');
+
+//display post in the dashboard
+router.get('/', withAuth, (req, res)=>{
+    //checking 
+    console.log(req.session);
+    //find all
+    Post.findAll({
+        where:{
+            //using ID to indicate
+            user_id:req.session.user_id
+        },
+        attributes: ['id', 'text', 'title', 'created_at'],
+        include: [
+            {
+                model:Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include :{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+
+})
