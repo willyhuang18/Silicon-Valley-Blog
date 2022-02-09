9812813f2848b2d8810f0258a3a8bf4418a638d9
wiily <willyhuang18@gmail.com>
2022-02-08 23:36:51 -0800

    create dashboard route

---
+++ controllers/dashboard.js
@@ -0,0 +1,5 @@
+//getting all model 
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
