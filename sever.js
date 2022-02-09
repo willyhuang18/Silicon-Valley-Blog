0b09b9d32516c1d5106fc6d7aaa2529378bc159c
wiily <willyhuang18@gmail.com>
2022-02-08 21:13:40 -0800

    start sever

--- sever.js
+++ sever.js
@@ -9,3 +9,14 @@ const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
