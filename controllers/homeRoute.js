a48f59ee33f966c35e046ef08304a81f1c1d4289
wiily <willyhuang18@gmail.com>
2022-02-08 21:51:43 -0800

    render all post

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -3,3 +3,26 @@ const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+//render all post 
+router.get('/', (req, res)=>{
+    //checking 
+    console.log(req.session);
+    //find all
+    Post.findAll({
+        attributes: ['id', 'text', 'title', 'created_at'],
+        include: [
+            {
+                model:Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include :{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+})
