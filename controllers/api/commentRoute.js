6fab4c1ec3b791d97434f3f3c4936f4206ee01a8
wiily <willyhuang18@gmail.com>
2022-02-08 20:59:12 -0800

    Get route for all comment

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -2,3 +2,15 @@
 const router = require('express').Router();
 //getting comment model
 const { Comment } = require('../../models');
+//importing withAuth utils
+const withAuth = require('../../utils/auth');
+
+//Get route for all comment
+router.get('/' ,(req, res) => {
+    Comment.findAll({})
+    .then(response => res.json(response))
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err); 
+    })
+})
