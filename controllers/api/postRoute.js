79a3141b402510ebdbfbba40eed701b172a2f5a1
wiily <willyhuang18@gmail.com>
2022-02-08 17:21:30 -0800

    Get route for all post

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -4,3 +4,29 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 //importing withAuth utils
 const withAuth = require('../../utils/auth');
+
+//Get route for all post
+router.get('/', (req,res) =>{
+    Post.findAll({
+        //query configuration
+        attributes: ['id', 'text', 'title', 'created_at'],
+        //first element is the column / function to order by, the second is the direction.
+        order: [['created_at', 'DESC']],
+
+        //include comment and user
+        include: [
+            {
+                model:Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include :{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+})
