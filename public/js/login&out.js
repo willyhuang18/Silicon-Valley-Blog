75b0aae9cae4e6d619d77c7766eb8f6a807f4fe8
wiily <willyhuang18@gmail.com>
2022-02-09 18:13:47 -0800

    settting up login js

---
+++ public/js/login&out.js
@@ -0,0 +1,29 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+
+    // Collect values from the login form
+    const email = document.querySelector('#loginEmail').value.trim();
+    const password = document.querySelector('#loginPassword').value.trim();
+  
+    if (email && password) {
+    // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+      // If successful, redirect the browser to the profile page
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+
+document.querySelector('.loginForm').addEventListener('submit', loginFormHandler);
