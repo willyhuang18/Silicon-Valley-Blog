a6d6f817a57e86c6ffd8dfd8a712950fc532c1fb
wiily <willyhuang18@gmail.com>
2022-02-09 20:29:05 -0800

    set Handlebars as the default template engine

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(routes);
 
