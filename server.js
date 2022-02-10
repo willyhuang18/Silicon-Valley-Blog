b483f6b96ce55b064710b883845e3b4e61443311
wiily <willyhuang18@gmail.com>
2022-02-09 20:26:31 -0800

    import helper function

--- server.js
+++ server.js
@@ -6,13 +6,22 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // connects session to sequelize Database
 const session = require('express-session');
+// helper function
+const helpers = require('./utils/helpers');
+
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
