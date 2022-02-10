3a75233d9cf0a89e84bd52e22f5804f8b6e6f1c5
wiily <willyhuang18@gmail.com>
2022-02-09 18:20:07 -0800

    async signup form

--- public/js/login&signup.js
+++ public/js/login&signup.js
@@ -28,4 +28,31 @@ async function loginFormHandler(event) {
 
 document.querySelector('.loginForm').addEventListener('submit', loginFormHandler);
 
+//signup form
+async function signupFormHandler(event) {
+  event.preventDefault();
 
+  const name = document.querySelector('#signUpName').value.trim();
+  const email = document.querySelector('#signUpEmail').value.trim();
+  const password = document.querySelector('#signUpPassword').value.trim();
+
+  if (name && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }); 
+  if (response.ok) {
+      console.log('success');
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler); 
