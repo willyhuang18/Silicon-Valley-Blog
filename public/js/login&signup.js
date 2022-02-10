b7ee05f0d9469aeaf857bbc0355e211557688694
wiily <willyhuang18@gmail.com>
2022-02-09 18:20:45 -0800

    ajust data

--- public/js/login&signup.js
+++ public/js/login&signup.js
@@ -55,4 +55,4 @@ async function signupFormHandler(event) {
   }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler); 
+document.querySelector('.signuPForm').addEventListener('submit', signupFormHandler); 
