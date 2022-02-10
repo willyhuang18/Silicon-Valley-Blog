98239fa12a815b30e431e3ab42d5d305393d718d
wiily <willyhuang18@gmail.com>
2022-02-09 18:48:09 -0800

    update form function

--- public/js/post.js
+++ public/js/post.js
@@ -24,3 +24,34 @@ async function newPostHandler(event) {
 }
 
 document.querySelector('.newPostForm').addEventListener('submit', newPostHandler);
+
+//update post function
+async function updateFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('#updateTitle').value.trim();
+    const text = document.querySelector('#updateText').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+  
+}
+
+document.querySelector('.updateForm').addEventListener('submit', updateFormHandler);
