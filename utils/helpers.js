024dd16e86b604731b652b6e4aed0eb8584f95bd
wiily <willyhuang18@gmail.com>
2022-02-09 22:54:12 -0800

    add some helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
-    format_plural: (word, amount) => {
+    format_comment: (word, amount) => {
         if (amount !== 1) {
           return `${word}s`;
         }
